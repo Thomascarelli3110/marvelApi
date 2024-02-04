1b78801fb5ee8e363ed5466a7eead8676efdc05b
thomas <carellithomas3110@gmail.com>
2024-02-04 12:00:27 -0300

    modifying changes and creating a router

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-const charactersMArvelAPI = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=f62bf23851431279216b492ff5fd2062&hash=fb9d6f13b82af99a133b6e97d4fafa5a&limit=50"
+import {  handleChangeRoute } from "./router.js";
 
-export async function getCharacters(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data
-}
-
-async function displayCharacters() {
-  try {
-    const root = document.querySelector(".root")
-    const characters = await getCharacters(charactersMArvelAPI)
-    const heroes = characters.data.results;
-    const data = `
-    
-      ${heroes.map(hero => (
-      `
-      <div class="characters-container">
-          <a href="#" target="_blank">
-           <h2>${hero.name}</h3>
-            <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" atl="${hero.name}" class="img-thumbnail">
-          </a>
-      </div>
-      `
-    )).join('')}
-    
-    `
-    root.innerHTML = data
-  } catch (error) {
-    console.error("Se a producido un error al traer la informacion de la API:", error);
-  }
-}
-
-displayCharacters();
-
-
-
+window.addEventListener('hashchange', handleChangeRoute)
